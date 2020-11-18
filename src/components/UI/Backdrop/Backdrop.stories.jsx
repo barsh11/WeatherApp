cc5e9b124eef8111e3bfe11c226a8fc3cfbe6fba
barsh11 <barsh11@gmail.com>
2020-11-18 13:37:16 +0200

    added backdrop

---
+++ src/components/UI/Backdrop/Backdrop.stories.jsx
@@ -0,0 +1,13 @@
+/* eslint-disable import/no-anonymous-default-export */
+import React from 'react';
+import Backdrop from './Backdrop';
+
+export default{
+    title: 'Backdrop',
+    component: Backdrop
+}
+
+const Template= args => <Backdrop {...args} />
+
+export const Default= Template.bind({});
+Default.args={}
