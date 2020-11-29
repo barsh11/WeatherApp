13bd71d5017dd36189d90ffda605fcc084444387
barsh11 <barsh11@gmail.com>
2020-11-29 15:23:26 +0200

    added params to axios-locations

--- src/services/axios-locations.js
+++ src/services/axios-locations.js
@@ -2,6 +2,9 @@ import axios from "axios";
 
 const instance = axios.create({
   baseURL: "http://dataservice.accuweather.com/locations/v1/cities/search",
+  params: {
+    apikey: 'HOhjKEnfGfhjEi30ka9lXzZqFU0FXXwF'
+  }
 });
 
 export default instance;
