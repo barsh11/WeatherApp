4a4c866c3a44f90ac0c94591adacd3aae187b9bd
barsh11 <barsh11@gmail.com>
2020-12-03 08:53:15 +0200

    added axios constants file

---
+++ src/constants/axios.js
@@ -0,0 +1,15 @@
+// API KEY
+export const API_KEY= 'jlfEqbKUbVAxazV5lRKyg9hy6LUGOGUG';
+
+// LOCATIONS
+export const BASE_URL_LOCATIONS= 'http://dataservice.accuweather.com/locations/v1/cities/autocomplete';
+
+// CONDITIONS
+export const BASE_URL_CONDITIONS= 'http://dataservice.accuweather.com/currentconditions/v1/';
+
+// DAILY
+export const BASE_URL_DAILY= 'http://dataservice.accuweather.com/forecasts/v1/daily/1day/';
+
+// FORECAST
+export const BASE_URL_FORECAST= 'http://dataservice.accuweather.com/forecasts/v1/hourly/12hour/';
+
